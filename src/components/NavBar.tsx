253c022c8782e254ab15f360366e215d008c989c
ndutagrace25 <ndutagrace25@gmail.com>
2024-02-27 14:37:05 +0300

    pushing all changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ const NavBar = () => {
       <nav className="navbar navbar-expand-lg ">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">
-            ChezaGame Logo
+            ChezaHive Logo
           </a>
           <button
             className="navbar-toggler"
