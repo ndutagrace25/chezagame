0a2c20125f8994b48428518d8d149f0ea312e566
ndutagrace25 <ndutagrace25@gmail.com>
2024-06-08 15:21:27 +0300

    reduced font size

--- src/components/PopularProductCard.tsx
+++ src/components/PopularProductCard.tsx
@@ -67,15 +67,15 @@ const PopularProductCard = ({
         </a>
       </div>
       <div className="mt-8 flex justify-start gap-2.5">
-        <img src={star} alt="rating" width={24} height={24} />
-        <p className="font-montserrat text-xl leading-normal text-slate-gray">
+        <img src={star} alt="rating" width={16} height={16} />
+        <p className="font-montserrat leading-normal text-slate-gray">
           (4.5)
         </p>
       </div>
-      <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">
+      <h3 className="mt-2 leading-normal font-semibold font-palanquin">
         {name}
       </h3>
-      <p className="mt-2 font-semibold font-montserrat text-gray-900 text-2xl leading-normal">
+      <p className="mt-2 font-semibold font-montserrat text-gray-900 leading-normal">
         {price}
       </p>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       xs: ["12px", "16px"],
       sm: ["14px", "20px"],
       base: ["16px", "19.5px"],
-      lg: ["18px", "21.94px"],
+      lg: ["16px", "20.94px"],
       xl: ["20px", "24.38px"],
       "2xl": ["24px", "29.26px"],
       "3xl": ["28px", "50px"],
